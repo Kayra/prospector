4280ac2c75273c97d06cdc9d9fb72410a8c33e24
Kayra <KayraAlat@gmail.com>
2016-06-01 10:50:08 +0100

    Added headers and convinience class to spider

--- app/spider.py
+++ app/spider.py
@@ -11,5 +11,19 @@ class ProspectorSpider(CrawlSpider):
 
     rules = (Rule(LinkExtractor(), callback="parse_item"))
 
+    def _string_joiner(self, data):
+        if len(data) > 1:
+            return '#'.join(data)
+        elif len(data) == 1:
+            return data[0]
+        else:
+            return None
+
     def parse_item(self, response):
-        pass
+
+        page_data = {}
+
+        for header_number in range(1, 4):
+            page_data["h" + header_number + "s"] = self._string_joiner(response.css("h" + header_number).extract())
+
+        return page_data
