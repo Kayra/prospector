23fa7869c47f77c66fddd81a3f57d3ee6a3a9205
Kayra <KayraAlat@gmail.com>
2016-09-24 23:53:39 +0100

    Added db handling for scraped pages

--- app/views.py
+++ app/views.py
@@ -21,9 +21,13 @@ def index():
         try:
             crawler = Crawler()
             domain_data = crawler.scrape_domain_data(url_to_prospect)
+
             pages_to_scrape = crawler.spider_site(domain_data.domain_url)
             pages_data = [crawler.scrape_page_data(page_to_scrape) for page_to_scrape in pages_to_scrape]
 
+            db.session.add(domain_data)
+            db.session.add_all(pages_data)
+            db.session.commit()
             # site_id = models.DomainData.query.filter_by(site_name=site_name).first().id
             # Ranker(site_id)
             return redirect(url_for('siteinspect', site_name=domain_data.site_name))
