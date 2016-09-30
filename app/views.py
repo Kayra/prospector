ce06227a33e359fd247d33576a516f7d13db4ef6
Kayra <KayraAlat@gmail.com>
2016-09-30 22:56:57 +0300

    Removed ambiguous exception handling

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, redirect, url_for
 
 from config import POSTS_PER_PAGE
 
@@ -18,26 +18,21 @@ def index():
 
         url_to_prospect = format_url(form.url.data)
 
-        try:
-            crawler = Crawler()
-            domain_data = crawler.scrape_domain_data(url_to_prospect)
+        crawler = Crawler()
+        domain_data = crawler.scrape_domain_data(url_to_prospect)
 
-            pages_to_scrape = crawler.spider_site(domain_data.domain_url)
-            pages_data = [crawler.scrape_page_data(page_to_scrape, domain_data) for page_to_scrape in pages_to_scrape]
+        pages_to_scrape = crawler.spider_site(domain_data.domain_url)
+        pages_data = [crawler.scrape_page_data(page_to_scrape, domain_data) for page_to_scrape in pages_to_scrape]
 
-            ranker = Ranker()
-            domain_data.ranking = ranker.rank_site(domain_data)
-            domain_data.level = ranker.domain_level_calculator(domain_data.ranking)
+        ranker = Ranker()
+        domain_data.ranking = ranker.rank_site(domain_data)
+        domain_data.level = ranker.domain_level_calculator(domain_data.ranking)
 
-            db.session.add(domain_data)
-            db.session.add_all(pages_data)
-            db.session.commit()
+        db.session.add(domain_data)
+        db.session.add_all(pages_data)
+        db.session.commit()
 
-            return redirect(url_for('siteinspect', site_name=domain_data.site_name))
-
-        except ValueError as error:
-            print(error)
-            flash("Invalid url")
+        return redirect(url_for('siteinspect', site_name=domain_data.site_name))
 
     return render_template("index.html", form=form)
 
