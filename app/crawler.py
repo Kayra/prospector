1fc2d90e2f4d4828d4efd0e88eaa54e51391d08d
Kayra <KayraAlat@gmail.com>
2016-10-09 00:04:32 +0300

    Refactored crawler

--- app/crawler.py
+++ app/crawler.py
@@ -1,15 +1,15 @@
+from bs4 import BeautifulSoup
+
+import urllib.parse as urlparse
+import urllib
+
 from app.models import DomainData, PageData
 from app.scrapers import DomainScraper, PageScraper
 from app.utils import extract_site_name
-import urllib.parse as urlparse
-import urllib
-from bs4 import BeautifulSoup
+from config import MAX_PAGES_TO_VISIT
 
 
 class Crawler():
-
-    def __init__(self):
-        self._MAX_PAGES_TO_VISIT = 60
 
     def get_html_soup(self, url):
         raw_html = urllib.request.urlopen(url).read()
@@ -99,7 +99,7 @@ class Crawler():
 
             current_url = urlparse.urljoin(domain_url, link['href'])
 
-            if urlparse.urlparse(current_url).netloc in urlparse.urlparse(domain_url).netloc and '#' not in current_url and current_url not in urls and len(urls) < self._MAX_PAGES_TO_VISIT and '@' not in current_url:
+            if urlparse.urlparse(current_url).netloc in urlparse.urlparse(domain_url).netloc and '#' not in current_url and current_url not in urls and len(urls) < MAX_PAGES_TO_VISIT and '@' not in current_url:
                 urls.append(current_url)
 
         return urls
--- config.py
+++ config.py
@@ -8,8 +8,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Pagination
 POSTS_PER_PAGE = 1
 
+# Ranking calculation constant
 DOMAIN_IMPORTANCE = 1.3
 
-# Don't need for this application, if user accounts are added in the future this needs to be re-enabled
+# Spidering limit
+MAX_PAGES_TO_VISIT = 60
+
+# Don't need for this application, if user accounts are added in the future this will be useful
 CSRF_ENABLED = False
 SECRET_KEY = 'you-will-never-guess'
