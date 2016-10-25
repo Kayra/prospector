6cfb90b5494c08de73d6bd0e537351f3ab26b349
Kayra <KayraAlat@gmail.com>
2016-10-25 22:21:55 +0100

    Changed DB config to postgres

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://prospector:password@localhost:5432/prospector'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
