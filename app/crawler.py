10ed5b4d92bbbf4fa4002ab7e705d6d79fbb08cd
Kayra <KayraAlat@gmail.com>
2016-12-26 00:51:10 +0000

    Fixed page data duplication bug

--- app/crawler.py
+++ app/crawler.py
@@ -71,8 +71,8 @@ class Crawler():
         schema_tag = PageScraper.schema_tag(page_html_soup)
         blog_location = PageScraper.blog_location(page_html_soup)
         number_of_internal_links = PageScraper.number_of_internal_links(page_html_soup, page_url)
-
-        page_data = PageData.query.filter_by(page_url).first()
+        print("HIT", page_url)
+        page_data = PageData.query.filter_by(page_url=page_url).first()
 
         if page_data:
             page_data.page_url = page_url,
