12d29d0541ec7912614766b7987c02c05123fcd0
Kayra <KayraAlat@gmail.com>
2017-01-09 23:10:39 +0000

    Update schema tag scraping

--- app/scrapers.py
+++ app/scrapers.py
@@ -82,7 +82,7 @@ class PageScraper():
 
     @staticmethod
     def schema_tags(page_html_soup):
-        return [schema_tag for schema_tag in page_html_soup.find_all('div') if schema_tag.get('itemtype')]
+        return [schema_tag.text for schema_tag in page_html_soup.find_all('div') if schema_tag.get('itemtype')]
 
     @staticmethod
     @no_tag_exception_handler
