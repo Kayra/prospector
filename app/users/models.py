2e2f7c8365b6d5f739d9fd561b98e88949cbf9cf
Kayra <KayraAlat@gmail.com>
2017-02-23 15:14:27 +0000

    Users - Added initial user model

--- app/users/models.py
+++ app/users/models.py
@@ -2,3 +2,12 @@ from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
 from app import db
 
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
