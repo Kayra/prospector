2db8cde00c462c55458497c79aa4c45b2b8606f1
Kayra <KayraAlat@gmail.com>
2017-02-24 15:27:37 +0000

    Users - Updated user model

--- app/users/models.py
+++ app/users/models.py
@@ -11,3 +11,10 @@ class User(db.Model):
 
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
+
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Coumn(db.String(100), nullable=False)
