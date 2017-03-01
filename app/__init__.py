f239513434e43e4047135e1acf62d3ae6fb931aa
Kayra <KayraAlat@gmail.com>
2017-03-01 16:36:45 +0000

    Users - Added user manager to init of app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_user import UserManager
 from flask_mail import Mail
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 mail = Mail(app)
+user_manager = UserManager(db, app)
 
 from app.prospector.views import prospector_blueprint
 app.register_blueprint(prospector_blueprint)
