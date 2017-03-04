ee2ee5574b4d8ca1d7c1594214edbedb5f89e006
Kayra <KayraAlat@gmail.com>
2017-03-04 16:41:48 +0000

    Users - Added tests and imports to user views

--- app/users/views.py
+++ app/users/views.py
@@ -1,16 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, app
+from flask_user import login_required
 
 users_blueprint = Blueprint('users', __name__)
 
+
 @app.route('/login')
 def login():
+    print("HIT LOGIN")
     pass
+
 
 @app.route('/logout')
 def logout():
+    print("HIT LOGOUT")
     pass
+
 
 @app.route('/profile/<username>')
 @login_required
 def profile(username):
+    print("HIT PROFILE ON USER", username)
     pass
