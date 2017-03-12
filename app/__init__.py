9314a32afb7812e592a702dbea1b3266224b3dd2
Kayra <KayraAlat@gmail.com>
2017-03-12 20:33:57 +0000

    Users - Clean up

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,14 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 mail = Mail(app)
 
-
-
 from app.prospector.views import prospector_blueprint
 app.register_blueprint(prospector_blueprint)
 
 from app.users.views import users_blueprint
 app.register_blueprint(users_blueprint)
 
-from app.prospector import views, models
 from app.users.models import User
-
 db_adapter = SQLAlchemyAdapter(db, User)
 user_manager = UserManager(db, app)
+
+from app.prospector import views, models
