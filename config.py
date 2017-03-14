e0107716abedfed3d148341821432a998b2dd8df
Kayra <KayraAlat@gmail.com>
2017-03-14 13:22:25 +0000

    Added base config class

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://prospector:password@localhost:5432/prospector'
@@ -16,4 +17,18 @@ MAX_PAGES_TO_VISIT = 60
 
 # Don't need for this application, if user accounts are added in the future this will be useful
 CSRF_ENABLED = False
-SECRET_KEY = 'you-will-never-guess'
+
+
+class Configuration:
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Pagination
+    SITES_PER_PAGE = 1
+
+    # Ranking calculation constant
+    DOMAIN_IMPORTANCE = 1.3
+
+    # Spidering limit
+    MAX_PAGES_TO_VISIT = 60
