cca5a40b5c924c0684e382b927a3598352566511
Kayra <KayraAlat@gmail.com>
2017-03-25 14:00:27 +0000

    Refactored user views

--- app/users/views.py
+++ app/users/views.py
@@ -1,24 +1,22 @@
 from flask import Blueprint
 from flask_user import login_required
 
-from app import app
-
 users_blueprint = Blueprint('users', __name__)
 
 
-@app.route('/login')
+@users_blueprint.route('/login')
 def login():
     print("HIT LOGIN")
     pass
 
 
-@app.route('/logout')
+@users_blueprint.route('/logout')
 def logout():
     print("HIT LOGOUT")
     pass
 
 
-@app.route('/profile/<username>')
+@users_blueprint.route('/profile/<username>')
 @login_required
 def profile(username):
     print("HIT PROFILE ON USER", username)
