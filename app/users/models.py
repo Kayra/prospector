7e979e2c773f41165c2825fbd19f585218dd2a35
Kayra <KayraAlat@gmail.com>
2017-04-23 18:34:36 +0100

    Added functions to user models

--- app/users/models.py
+++ app/users/models.py
@@ -1,6 +1,7 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_user import UserMixin
 
-from app import db
+from app import login_manager, db
 
 
 class User(UserMixin, db.Model):
@@ -13,6 +14,21 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError("The password is not a readable attribute.")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     def __repr__(self):
         return '<User %r>' % (self.username)
