e0f9fa86d41f694618f43df98f36b25d3e146721
Kayra <KayraAlat@gmail.com>
2017-04-23 18:50:15 +0100

    Login fully functioning

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
-from flask_user import login_required
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import login_required, login_user
 
 from app import db
 from app.users.forms import LoginForm, RegistrationForm
@@ -30,10 +30,21 @@ def register():
         return render_template("users/register.html", form=registration_form)
 
 
-@users_blueprint.route('/login')
+@users_blueprint.route('/login', methods=["GET", "POST"])
 def login():
 
     login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+
+        user = User.query.filter_by(email=login_form.email.data).first()
+
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember_me.data)
+            return redirect(request.args.get("next") or url_for("prospector.index"))
+
+        else:
+            flash("Invalid username or password")
 
     return render_template("users/login.html", form=login_form)
 
@@ -49,4 +60,3 @@ def logout():
 def profile(username):
     print("HIT PROFILE ON USER", username)
     pass
-
