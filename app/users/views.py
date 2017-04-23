fcc4706649b7ccf1fdd793dc3d3307a79a90b37a
Kayra <KayraAlat@gmail.com>
2017-04-23 18:44:05 +0100

    Registration fully functioning

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_user import login_required
 
+from app import db
 from app.users.forms import LoginForm, RegistrationForm
+from app.users.models import User
 
 users_blueprint = Blueprint('users', __name__)
+
+
+@users_blueprint.route('/register', methods=["GET", "POST"])
+def register():
+
+    registration_form = RegistrationForm()
+
+    if registration_form.validate_on_submit():
+
+        user = User(email=registration_form.email.data,
+                    username=registration_form.username.data,
+                    password=registration_form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("Your account has been created. Please log in.")
+
+        return redirect(url_for("users.login"))
+
+    else:
+        return render_template("users/register.html", form=registration_form)
 
 
 @users_blueprint.route('/login')
@@ -26,10 +50,3 @@ def profile(username):
     print("HIT PROFILE ON USER", username)
     pass
 
-
-@users_blueprint.route('/register')
-def register():
-
-    registration_form = RegistrationForm()
-
-    return render_template("users/register.html", form=registration_form)
