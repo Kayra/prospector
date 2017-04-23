1763e78ba764b783f01515d6241e7f6fc2dafebe
Kayra <KayraAlat@gmail.com>
2017-04-23 18:31:08 +0100

    Added LoginManager to app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 
 from config import configurations
 
 db = SQLAlchemy()
-mail = Mail()
 bootstrap = Bootstrap()
+mail = Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "users.login"
 
 
 def create_app(configuration_name):
@@ -17,8 +22,9 @@ def create_app(configuration_name):
     configurations[configuration_name].init_app(app)
 
     db.init_app(app)
-    mail.init_app(app)
     bootstrap.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
 
     from app.prospector.views import prospector_blueprint
     app.register_blueprint(prospector_blueprint)
