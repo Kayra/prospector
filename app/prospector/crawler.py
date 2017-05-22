ce39617dba27d2fdcef9ab04e22e6da4cc4e413f
Kayra <KayraAlat@gmail.com>
2017-05-22 19:02:08 +0100

    Refactor - Unclassed crawler

--- app/prospector/crawler.py
+++ app/prospector/crawler.py
@@ -13,94 +13,97 @@ from app.prospector.utils import extract_site_name
 MAX_PAGES_TO_VISIT = 60
 
 
-class Crawler():
+def _get_html_soup(url):
+    raw_html = request.urlopen(url).read()
+    return BeautifulSoup(raw_html, "html.parser")
 
-    def _get_html_soup(self, url):
-        raw_html = request.urlopen(url).read()
-        return BeautifulSoup(raw_html, "html.parser")
 
-    def _get_page_contents(self, domain_url, url_suffix):
+def _get_page_contents(domain_url, url_suffix):
 
-        full_url = parse.urljoin(domain_url, url_suffix)
+    full_url = parse.urljoin(domain_url, url_suffix)
 
-        try:
-            raw_data = request.urlopen(full_url).read()
-            if isinstance(raw_data, str) and len(raw_data) < 500000:
-                return raw_data.decode('utf-8')
-        except error.HTTPError as error_message:
-            print('{} for {}'.format(error_message, full_url))
+    try:
+        raw_data = request.urlopen(full_url).read()
+        if isinstance(raw_data, str) and len(raw_data) < 500000:
+            return raw_data.decode('utf-8')
+    except error.HTTPError as error_message:
+        print('{} for {}'.format(error_message, full_url))
 
-    def scrape_domain_data(self, domain_url):
 
-        domain_html_soup = self._get_html_soup(domain_url)
+def scrape_domain_data(domain_url):
 
-        robots_txt = self._get_page_contents(domain_url, 'robots.txt')
-        sitemap_xml = self._get_page_contents(domain_url, 'sitemap.xml')
-        google_analytics = DomainScraper.scrape_google_analytics(domain_html_soup)
-        bing_analytics = DomainScraper.scrape_bing_analytics(domain_html_soup)
+    domain_html_soup = _get_html_soup(domain_url)
 
-        domain_data = DomainData.query.filter_by(domain_url=domain_url).first()
+    robots_txt = _get_page_contents(domain_url, 'robots.txt')
+    sitemap_xml = _get_page_contents(domain_url, 'sitemap.xml')
+    google_analytics = DomainScraper.scrape_google_analytics(domain_html_soup)
+    bing_analytics = DomainScraper.scrape_bing_analytics(domain_html_soup)
 
-        if not domain_data:
-            domain_data = DomainData(domain_url=domain_url,
-                                     site_name=extract_site_name(domain_url))
+    domain_data = DomainData.query.filter_by(domain_url=domain_url).first()
 
-        domain_data.robots_txt = robots_txt
-        domain_data.sitemap_xml = sitemap_xml
-        domain_data.google_analytics = google_analytics
-        domain_data.bing_analytics = bing_analytics
+    if not domain_data:
+        domain_data = DomainData(domain_url=domain_url,
+                                 site_name=extract_site_name(domain_url))
 
-        return domain_data
+    domain_data.robots_txt = robots_txt
+    domain_data.sitemap_xml = sitemap_xml
+    domain_data.google_analytics = google_analytics
+    domain_data.bing_analytics = bing_analytics
 
-    def scrape_page_data(self, page_url, domain):
+    return domain_data
 
-        page_html_soup = self._get_html_soup(page_url)
 
-        page_data = PageData.query.filter_by(page_url=page_url).first()
+def scrape_page_data(page_url, domain):
 
-        if not page_data:
-            page_data = PageData()
-            page_data.page_url = page_url
-            page_data.domain_site = domain
+    page_html_soup = _get_html_soup(page_url)
 
-        page_data.h1_tags = PageScraper.header_tags(page_html_soup, 'h1')
-        page_data.h2_tags = PageScraper.header_tags(page_html_soup, 'h2')
-        page_data.h3_tags = PageScraper.header_tags(page_html_soup, 'h3')
-        page_data.alt_tags = PageScraper.alt_tags(page_html_soup)
-        page_data.meta_descriptions = PageScraper.meta_descriptions(page_html_soup)
-        page_data.title_text = PageScraper.title_text(page_html_soup)
-        page_data.view_state = PageScraper.view_state(page_html_soup)
-        page_data.pagination = PageScraper.pagination(page_html_soup)
-        page_data.iframe_content = PageScraper.iframe_content(page_html_soup)
-        page_data.flash_attribute = PageScraper.flash_attribute(page_html_soup)
-        page_data.no_index_no_follow_attribute = PageScraper.no_index_no_follow_attribute(page_html_soup)
-        page_data.schema_tags = PageScraper.schema_tags(page_html_soup)
-        page_data.blog_locations = PageScraper.blog_locations(page_html_soup)
-        page_data.number_of_internal_links = PageScraper.number_of_internal_links(page_html_soup, page_url)
+    page_data = PageData.query.filter_by(page_url=page_url).first()
 
-        return page_data
+    if not page_data:
+        page_data = PageData()
+        page_data.page_url = page_url
+        page_data.domain_site = domain
 
-    def spider_site(self, domain_url):
+    page_data.h1_tags = PageScraper.header_tags(page_html_soup, 'h1')
+    page_data.h2_tags = PageScraper.header_tags(page_html_soup, 'h2')
+    page_data.h3_tags = PageScraper.header_tags(page_html_soup, 'h3')
+    page_data.alt_tags = PageScraper.alt_tags(page_html_soup)
+    page_data.meta_descriptions = PageScraper.meta_descriptions(page_html_soup)
+    page_data.title_text = PageScraper.title_text(page_html_soup)
+    page_data.view_state = PageScraper.view_state(page_html_soup)
+    page_data.pagination = PageScraper.pagination(page_html_soup)
+    page_data.iframe_content = PageScraper.iframe_content(page_html_soup)
+    page_data.flash_attribute = PageScraper.flash_attribute(page_html_soup)
+    page_data.no_index_no_follow_attribute = PageScraper.no_index_no_follow_attribute(page_html_soup)
+    page_data.schema_tags = PageScraper.schema_tags(page_html_soup)
+    page_data.blog_locations = PageScraper.blog_locations(page_html_soup)
+    page_data.number_of_internal_links = PageScraper.number_of_internal_links(page_html_soup, page_url)
 
-        urls = [domain_url]
+    return page_data
 
-        page_html_soup = self._get_html_soup(domain_url)
 
-        for link in page_html_soup.findAll('a', href=True):
+def spider_site(self, domain_url):
 
-            current_url = parse.urljoin(domain_url, link['href'])
+    urls = [domain_url]
 
-            if parse.urlparse(current_url).netloc in parse.urlparse(domain_url).netloc and \
-               '#' not in current_url and \
-               current_url not in urls and \
-               len(urls) < MAX_PAGES_TO_VISIT and \
-               '@' not in current_url:
-                urls.append(current_url)
+    page_html_soup = _get_html_soup(domain_url)
 
-        return urls
+    for link in page_html_soup.findAll('a', href=True):
 
-    def crawl_site(self, domain_url):
+        current_url = parse.urljoin(domain_url, link['href'])
 
-        self.scrape_domain_data(domain_url)
-        for page_url in self.spider_site(domain_url):
-            self.scrape_page_data(page_url)
+        if parse.urlparse(current_url).netloc in parse.urlparse(domain_url).netloc and \
+           '#' not in current_url and \
+           current_url not in urls and \
+           len(urls) < MAX_PAGES_TO_VISIT and \
+           '@' not in current_url:
+            urls.append(current_url)
+
+    return urls
+
+
+def crawl_site(domain_url):
+
+    scrape_domain_data(domain_url)
+    for page_url in spider_site(domain_url):
+        scrape_page_data(page_url)
