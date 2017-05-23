4a27a06cf8bc10e5d42dfb9d1fd35fae87eb0716
Kayra <KayraAlat@gmail.com>
2017-05-23 22:28:07 +0100

    Refactor - Updated ranker import

--- app/prospector/views.py
+++ app/prospector/views.py
@@ -28,7 +28,6 @@ def index():
         pages_to_scrape = crawler.spider_site(domain_data.domain_url)
         pages_data = [crawler.scrape_page_data(page_to_scrape, domain_data) for page_to_scrape in pages_to_scrape]
 
-        ranker = Ranker()
         domain_data.ranking = ranker.rank_site(domain_data)
         domain_data.level = ranker.domain_level_calculator(domain_data.ranking)
 
