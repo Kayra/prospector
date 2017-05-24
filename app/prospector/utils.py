3550a5d44ad41fcd4d9fce664b5584fca10a8357
Kayra <KayraAlat@gmail.com>
2017-05-24 09:49:51 +0100

    User-scraping - Spacing

--- app/prospector/utils.py
+++ app/prospector/utils.py
@@ -4,10 +4,13 @@ from app.prospector.models import DomainData
 
 
 def format_url(url):
+
     if 'www.' not in url and 'http://' not in url:
         return 'http://www.' + url
+
     elif 'http://' not in url:
         return 'http://' + url
+
     else:
         return url
 
