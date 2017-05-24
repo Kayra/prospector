0975beb0da8f9d35e751b9b6ec1f9c696dd422f5
Kayra <KayraAlat@gmail.com>
2017-05-24 23:26:58 +0100

    User-scores - Implemented create_default_domain_scores

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app, db
-from app.prospector.models import DomainScores, PageScores
+from app.prospector.utils import create_default_domain_scores, create_default_page_scores
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 
@@ -19,11 +19,8 @@ def db_create():
 
     db.create_all()
 
-    default_domain_scores = DomainScores(google_analytics=9,
-                                         bing_analytics=8,
-                                         robots_txt=9,
-                                         sitemap_xml=9,
-                                         owner=None)
+    default_domain_scores = create_default_domain_scores()
+    default_domain_scores.owner = None
 
     default_page_scores = PageScores(h1_tags=9,
                                      h2_tags=8,
