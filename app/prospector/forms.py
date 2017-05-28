76199d242f6fef49bd93d103409751e76f93a286
Kayra <KayraAlat@gmail.com>
2017-05-28 14:43:11 +0100

    User-scores - Added initial domain scores form

--- app/prospector/forms.py
+++ app/prospector/forms.py
@@ -1,7 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class UrlEntry(FlaskForm):
     url = StringField('url', validators=[DataRequired()])
+
+
+class DomainScores(FlaskForm):
+
+    google_analytics = IntegerField('google_analytics', validators=[DataRequired()])
+
+    submit = SubmitField("Save")
+    # google_analytics = db.Column(db.Integer)
+    # bing_analytics = db.Column(db.Integer)
+    # robots_txt = db.Column(db.Integer)
+    # sitemap_xml = db.Column(db.Integer)
