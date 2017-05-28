1790e43ca36692b9ade9c5f40f28262b5a59ab13
Kayra <KayraAlat@gmail.com>
2017-05-28 14:57:19 +0100

    User-scores - Added score validators

--- app/prospector/forms.py
+++ app/prospector/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 
 class UrlEntry(FlaskForm):
@@ -9,9 +9,9 @@ class UrlEntry(FlaskForm):
 
 class DomainScoresForm(FlaskForm):
 
-    google_analytics = IntegerField('google_analytics', validators=[DataRequired()])
-    bing_analytics = IntegerField('bing_analytics', validators=[DataRequired()])
-    robots_txt = IntegerField('robots_txt', validators=[DataRequired()])
-    sitemap_xml = IntegerField('sitemap_xml', validators=[DataRequired()])
+    google_analytics = IntegerField('google_analytics', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    bing_analytics = IntegerField('bing_analytics', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    robots_txt = IntegerField('robots_txt', validators=[DataRequired(), NumberRange(min=1, max=10)])
+    sitemap_xml = IntegerField('sitemap_xml', validators=[DataRequired(), NumberRange(min=1, max=10)])
 
     submit = SubmitField("Save")
