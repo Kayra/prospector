1f175686b046c11339232937178289a0ecb7adb5
Kayra <KayraAlat@gmail.com>
2017-08-05 18:35:09 +0300

    Tests - Added initial page test structure

--- app/prospector/tests/scraper_tests.py
+++ app/prospector/tests/scraper_tests.py
@@ -20,3 +20,15 @@ class TestDomainScrapers(unittest.TestCase):
 
     def test_bing_analytics_valid_strings(self):
         self.assertEquals(True, False)
+
+
+class TestPageScrapers(unittest.TestCase):
+
+    def test_header_no_valid_string(self):
+        self.assertEquals(True, False)
+
+    def test_header_invalid_strings(self):
+        self.assertEquals(True, False)
+
+    def test_header_valid_strings(self):
+        self.assertEquals(True, False)
