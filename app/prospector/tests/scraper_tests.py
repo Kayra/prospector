95775a2882a95947b6033de414da5a26eca9c3bb
Kayra <KayraAlat@gmail.com>
2017-08-05 19:11:20 +0300

    Tests - Added blog locations structure

--- app/prospector/tests/scraper_tests.py
+++ app/prospector/tests/scraper_tests.py
@@ -143,3 +143,15 @@ class TestSchemaTagsScraper(unittest.TestCase):
 
     def test_schema_tags_valid_strings(self):
         self.assertEquals(True, False)
+
+
+class TestBlogLocationsScraper(unittest.TestCase):
+
+    def test_blog_locations_no_valid_string(self):
+        self.assertEquals(True, False)
+
+    def test_blog_locations_invalid_strings(self):
+        self.assertEquals(True, False)
+
+    def test_blog_locations_valid_strings(self):
+        self.assertEquals(True, False)
