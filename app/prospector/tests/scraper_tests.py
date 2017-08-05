9d2c1f807c599d32c4219e37ae937c5f861ff5bc
Kayra <KayraAlat@gmail.com>
2017-08-05 18:33:08 +0300

    Tests - Added domain test structure

--- app/prospector/tests/scraper_tests.py
+++ app/prospector/tests/scraper_tests.py
@@ -1,7 +1,22 @@
 import unittest
 
 
-class TestScrapers(unittest.TestCase):
+class TestDomainScrapers(unittest.TestCase):
 
-    def test(self):
-        self.assertEquals(True, True)
+    def test_google_analytics_no_valid_string(self):
+        self.assertEquals(True, False)
+
+    def test_google_analytics_invalid_strings(self):
+        self.assertEquals(True, False)
+
+    def test_google_analytics_valid_strings(self):
+        self.assertEquals(True, False)
+
+    def test_bing_analytics_no_valid_string(self):
+        self.assertEquals(True, False)
+
+    def test_bing_analytics_invalid_strings(self):
+        self.assertEquals(True, False)
+
+    def test_bing_analytics_valid_strings(self):
+        self.assertEquals(True, False)
