800c1ac18c98e11210df7068c1657e5fd64bb88b
Kayra <KayraAlat@gmail.com>
2017-08-13 00:21:18 +0300

    Testing - Added header tag test string

--- app/prospector/tests/scraper_test_html.py
+++ app/prospector/tests/scraper_test_html.py
@@ -49,3 +49,12 @@ class TestHTML:
             </script>
             </body>
         """
+
+        self.header_tag_valid_html = """
+            <h1>Test</h1>
+            <h2>Test</h2>
+            <h3>Test</h3>
+            <h4>Test</h4>
+            <h5>Test</h5>
+            <h6>Test</h6>
+        """
